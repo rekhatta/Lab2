a1f0846bb6142ef12632df17deaa536efcd2b5d7
Robert K <Robert K>
2016-02-05 12:08:39 -0500

    changes

--- main.c
+++ main.c
@@ -22,11 +22,42 @@
  For more information about my classes, my research, and my books, see
  http://users.ece.utexas.edu/~valvano/
  */
-
+#include <stdint.h>
+#include "inc/tm4c123gh6pm.h"
+#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
+#define PF0       (*((volatile uint32_t *)0x40025004))
+#define PF4       (*((volatile uint32_t *)0x40025040))
+#define SWITCHES  (*((volatile uint32_t *)0x40025044))
+#define SW1       0x10                      // on the left side of the Launchpad board
+#define SW2       0x01                      // on the right side of the Launchpad board
+#define SYSCTL_RCGC2_GPIOF      0x00000020  // port F Clock Gating Control
+#define RED       0x02
+#define BLUE      0x04
+#define GREEN     0x08
 #include <stdio.h>
 #include <stdint.h> // C99 variable types
 #include "inc/tm4c123gh6pm.h"
 void Output_Init(void);
+void PortF_Init(void){ volatile uint32_t delay;
+  SYSCTL_RCGCGPIO_R |= 0x00000020;  // 1) activate clock for Port F
+  delay = SYSCTL_RCGCGPIO_R;        // allow time for clock to start
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
+  // only PF0 needs to be unlocked, other bits can't be locked
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on PF7-0
+  GPIO_PORTF_PUR_R = 0x11;          // enable pull-up on PF0 and PF4
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital I/O on PF4-0
+}
+uint32_t PortF_Input(void){     
+  return (GPIO_PORTF_DATA_R&0x11);  // read PF4,PF0 inputs
+}
+
+void PortF_Output(uint32_t data){ // write PF3-PF1 outputs
+  GPIO_PORTF_DATA_R = data;      
+}
 void BookExamples(void){ // examples from the book
   int8_t cc = 0x56; // (‘V’)
   int32_t xx = 100;
@@ -37,10 +68,21 @@ void BookExamples(void){ // examples from the book
   printf("yy = %d %#x\n",yy,yy);	//yy = -100 0xffffff9c
   printf("zz = %e %E %f %g %3.2f\n",zz,zz,zz,zz,zz);	//zz = 3.14159 3.14
 }
+void GPIO_Init(void){  
+  SYSCTL_RCGCGPIO_R |= 0x08;        // 1) activate port D
+  while((SYSCTL_PRGPIO_R&0x08)==0){};   // allow time for clock to stabilize
+                                    // 2) no need to unlock PD3-0
+  GPIO_PORTD_AMSEL_R &= ~0x0F;      // 3) disable analog functionality on PD3-0
+  GPIO_PORTD_PCTL_R &= ~0x0000FFFF; // 4) GPIO
+  GPIO_PORTD_DIR_R |= 0x0F;         // 5) make PD3-0 out
+  GPIO_PORTD_AFSEL_R &= ~0x0F;      // 6) regular port function 
+  GPIO_PORTD_DEN_R |= 0x0F;         // 7) enable digital I/O on PD3-0
+}
 int main(void){ int32_t i,n;
   Output_Init();              // initialize output device
   BookExamples();
   n = 0;
+	GPIO_Init();
   while(1){
     //printf("\ni="); 
     for(i=0; i<5; i++){
